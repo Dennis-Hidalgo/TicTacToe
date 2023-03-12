d6f2b5a90771e238c3f90aaac6d3851eeeb4a615
Dennis Hidalgo <dennisf00@hotmail.com>
2023-03-12 17:14:29 +0100

    entrega TicTacToe

--- tictactoe.js
+++ tictactoe.js
@@ -1,22 +1,11 @@
-/**
- * Todas las casillas tienen la clase .casilla
- * 
- */
 
-/**
- * Almacenamos todas las casillas, es decir, todos los divs que tienen clase 'casilla'
- * En total tenemos 9 casillas que van desde la 0 hasta la 8
- */
 let casillas = document.getElementsByClassName("casilla");
 
 /**
  * Creamos un array con arrays que contienen todas las combinaciones ganadoras
- * 
- * [0] => [0, 1, 2]
- * [1] => [3, 4, 5]
- * ...
  */
-let combinacionesGanadoras = [
+
+const combinacionesGanadoras = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -27,130 +16,264 @@ let combinacionesGanadoras = [
     [2, 5, 8]
 ];
 
-/**
- * Recorrer las casillas que tengo en array casillas
- * Comprobar el contenido de cada una
- */
-
-/**
- * Utilizo el array posicionesLlenas para introducir aquellas posiciones que contienen
- * un texto igual a 'X'
- * 
- * Al realizar un push lo que hago es introducir en el array, el numero de la posicion
- */
-let posicionesLlenas = [];
-for (let i = 0; i < casillas.length; i++) {
-    console.log('La casilla numero ' + i + ' contiene: ' + casillas[i].innerHTML);
-    if (casillas[i].innerHTML == 'X') {
-        posicionesLlenas.push(i);
-    }
-}
-console.log(posicionesLlenas);
-
-/**
- * Una vez tengo un array con las posiciones que contienen una 'X',
- * me interesa poder comparar si en el contenido de 'posicionesLlenas' esta incluido
- * alguna de las combinaciones de 'combinacionesGanadoras'.
- * 
- * En este caso en 'posicionesLlenas' tenemos:
- * [0] ---> 0
- * [1] ---> 1
- * [2] ---> 2
- * [3] ---> 5
- * 
- * En este caso en 'combinacionesGanadoras' tenemos:
- * [0] ---> [0, 1, 2]           // CORRECTA
- */
-
-/**
- * CONTENIDO NUEVO
- */
-
 let turno = true;
 let fichas1 = [];
 let fichas2 = [];
-let ganador = false;
-let continuar = false;
-let contador = 0;
+let victoria = false;
+let coincidencias = 0;
+let victoriasX;
+let victoriasO;
+let empates = 0;
+let cuentaAtras = 16;
+let contarTurnos = 0;
+let cambioturno;
+let reduccion;
+let tiempo = document.getElementById('temporizador');
+let jugador1 = document.getElementsByClassName('nombre')[0];
+let jugador2 = document.getElementsByClassName('nombre')[1];
+let cont1 = document.getElementsByClassName('contador')[0];
+let cont2 = document.getElementsByClassName('contador')[1];
+let cont3 = document.getElementsByClassName('contador')[2];
+document.getElementsByClassName('nombre')[2].textContent = 'Empates:';
 
+
+function display(){
+    let lineasV = [0, 1, 3, 4, 6, 7];
+    let lineasH =0;
+    do {
+        casillas[lineasH].style.borderBottom = '5px dashed black';
+        lineasH++;
+    } while (lineasH<6);
+    for  (pos in lineasV){
+        casillas[lineasV[pos]].style.borderRight = '5px dashed black';
+    }
+}
+/**
+ * Funcion que pide nombre de los 2 jugadores y reinicia las estadisticas
+ * @param 
+ * @returns
+ */
+function empezarJuego() {
+    jugador1.textContent = prompt('introduce el nombre del primer jugador');
+    jugador2.textContent = prompt('introduce el nombre del segundo jugador');
+    reiniciarEstadisticas();
+    console.log(tiempo.innerHTML);
+    display();
+}
+
+/**
+ * funcion que reinicia el tablero devolviendo los atributos 'onclick' y vaciando el contenido de cada casiila
+ * @param
+ * @returns
+ */
+function siguientePartida() {
+    for (let k = 0; k < casillas.length; k++) {
+        casillas[k].style.color = "black";
+        casillas[k].setAttribute('onclick', `agregarFicha(${k})`);
+        casillas[k].innerHTML = "";
+    }
+    victoria = false;
+    coincidencias = 0;
+    ganador = false;
+    i = 0;
+    j = 0;
+    fichas1 = [];
+    fichas2 = [];
+    contarTurnos = 0;
+    mostrarTemporizador();
+}
+
+/**
+ * funcion que permite reiniciar las estadisticas, sin necesidad de introducir de nuevo los nombres
+ * @param
+ * @returns
+ */
+function reiniciarEstadisticas() {
+    siguientePartida();
+    turno = true;
+    victoriasO = 0;
+    victoriasX = 0;
+    empates = 0;
+    cont1.innerHTML = victoriasX;
+    cont2.innerHTML = victoriasO;
+    cont3.innerHTML = empates;
+}
+
+/**
+ * Funcion que comprueba si las fichas del jugador coinciden con una de las combinaciones ganadoras
+ * @param {Array} fichas Array que contiene las fichas a evaluar
+ * @returns
+ */
 function validarVictoria(fichas = []) {
     let i = 0;
     let j = 0;
+    let ganador = false;
     fichas.sort();
-
     do {
-        console.log(combinacionesGanadoras[i]);  
+        coincidencias = 0;
+        j = 0;
+        // Recorro la longitud del array, junto con cada array de las combinaciones ganadoras
         do {
-            
             if (combinacionesGanadoras[i].includes(fichas[j])) {
-
-                contador = contador + 1;
+                coincidencias++;
             }
-
-            console.log("fichas: " + fichas[j]);
-            console.log("j: " + j);
-            console.log(contador);
-            console.log(continuar);
-            j = j + 1;
-
-            if (contador >= 3) {
+            j++;
+            // Si las coincidencias llegan a 3, es que mis fichas contienen una de las conmbinaciones ganadoras
+            // 'ganador' valida que existe una victoria
+            if (coincidencias == 3) {
                 ganador = true;
+                victoria = true;
             }
         } while (j < fichas.length && ganador == false)
-
-
+        // En caso de no haber ganador, voy al siguiente array de combinaciones ganadoras
         if (ganador == false) {
-            i = i + 1;
-            contador = 0;
-            j=0;
+            i++;
+            j = 0;
+        } else {
+            // Si existe una victoria, recojo el array que contiene la combinacion ganadora y lo resalto
+            victoria = true;
+            console.log(combinacionesGanadoras[i])
+            for (pintar in combinacionesGanadoras[i]) {
+                casillas[combinacionesGanadoras[i][pintar]].style.color = 'red';
+            }
         }
     } while (i < combinacionesGanadoras.length && ganador == false)
 }
 
-// validarVictoria();
+/**
+ * Funcion que hace distincion entre jugador 1(X) y jugador 2(O)
+ * @param {Array} fichasJug Array que contiene las casillas marcadas de cada jugador
+ * @param {Element} jugadorAux Elemento que contiene el nombre del jugador
+ * @param {int} numeroAux parametro para referenciar de nuevo la posicion de la casilla
+ * @return
+ */
+function distinguirFicha(fichasJug = [], jugadorAux, numeroAux = 0) {
+    casillas[numeroAux].removeAttribute('onclick')
+    fichasJug.push(numeroAux);
+    detenerIntervalo();
+    if (fichasJug.length >= 3) {
+        validarVictoria(fichasJug);
+        if (victoria) {
+            cuentaAtras = 16;
+            tiempo.innerHTML = `Partida terminada, una victoria mas para ${jugadorAux.textContent}`;
+            // En caso de existir victoria, quito todos losa tributos 'onclick' para que no se pueda introducir mas fichas
+            for (let i = 0; i < casillas.length; i++) {
+                casillas[i].removeAttribute('onclick')
+            }
+            
+        }
+    }
+}
 
+function pintarGanador(){
+    if (victoriasX>victoriasO){
+        cont1.style.color = 'green';
+        cont2.style.color = 'red';
+    } else if(victoriasX<victoriasO) {
+        cont1.style.color = 'red';
+        cont2.style.color = 'green';
+    } else if (victoriasO == victoriasX){
+        cont1.style.color = 'gray';
+        cont2.style.color = 'gray';
+    }
+}
+/**
+ * funcion que agrega las fichas en el tablero 
+ * @param {int} numero hace referencia a la posicion de la casilla en la que agrego la ficha
+ * @returns
+ */
 function agregarFicha(numero) {
-    console.log('Has hecho clic en la casilla ' + numero)
-
-    /**
-     * Cuando se activa esta funcion por el evento del click
-     * es necesario eliminar el clic del div
-     */
-
-    do {
+    if (!victoria) {
         if (turno) {
             casillas[numero].innerHTML = 'X';
-            casillas[numero].removeAttribute('onclick')
             turno = false;
-            fichas1.push(numero);
-            if (fichas1.length >= 3) {
-                validarVictoria(fichas1);
+            distinguirFicha(fichas1, jugador1, numero);
+            if (victoria) {
+                victoriasX++;
+                pintarGanador();
+                cont1.innerHTML = victoriasX;
+                // Cambio de turno para que el perdedor, sea el que empieza en la siguiente ronda
+                turno = false;
             }
-            console.log(ganador);
-        
         } else {
             casillas[numero].innerHTML = 'O';
-            casillas[numero].removeAttribute('onclick')
             turno = true;
-            fichas2.push(numero);
-            // validarVictoria(fichas2);
-            console.log(ganador);
-
+            distinguirFicha(fichas2, jugador2, numero);
+            if (victoria) {
+                victoriasO++;
+                pintarGanador();
+                cont2.innerHTML = victoriasO;
+                // Cambio de turno para que el perdedor, sea el que empieza en la siguiente ronda                
+                turno = true;
+            }
         }
-
-    } while (ganador);
-    // }
-    console.log(fichas1);
-    console.log(fichas2);
+        contarTurnos++;
+        console.log(contarTurnos);
+        // Si el contador de turnos llega a 9, y esta ultima jugada no da victoria, el contador de empates aumenta
+        if (!victoria && contarTurnos == 9) {
+            empates = empates + 1;
+            cont3.innerHTML = empates;
+            tiempo.innerHTML = `¡Empate!`;
+        } else if (!victoria) {
+            cuentaAtras = 16;
+            mostrarTemporizador();
+        }
+    }
 }
 
 /**
- * Para acabar el juego necesitamos:
- * 1. Colocar ficha
- * 2. Comprobar en cada insercion de ficha si se ha ganado el juego
- * 3. Cambiar turno
- * 4. Cuando hay ganador, mostrar mensaje
- * 
- * OPCIONES EXTRA:
- * 1. Generar un contador de victorias y resetear el tablero
+ * Funcion que reduce el temporizador y muestra su contenido cada vez que se ejecuta
  */
+function reducirTemporizador() {
+    let jugAux = "";
+    if (turno) {
+        jugAux = jugador1.textContent;
+    } else {
+        jugAux = jugador2.textContent;
+    }
+    cuentaAtras = cuentaAtras - 1;
+    tiempo.innerHTML = `Turno de ${jugAux}, cambio automatico en ${cuentaAtras}`;
+    if (cuentaAtras == 0) {
+        cuentaAtras = 16;
+    }
+}
+
+/**
+ * funcion que ejecuta la reduccion cada segundo
+ */
+function imprimirNumero() {
+    reduccion = setInterval(reducirTemporizador, 1000);
+}
+
+/**
+ * Funcion que muestra la cuenta atras, y cambia de turno tras el intervalo marcado en milisegundos
+ * @param
+ * @returns
+ */
+function mostrarTemporizador() {
+    cambioturno = setInterval(cambiarTurno, 16000);
+    imprimirNumero();
+}
+
+/**
+ * funcion que realiza el cambio de turno para ejecutarse en la funcion mostrarTemporizador()
+ * @param
+ * @returns
+ */
+function cambiarTurno() {
+    if (turno) {
+        turno = false;
+    } else {
+        turno = true;
+    }
+}
+
+/**
+ * Funcion que detiene ambos intervalos
+ * @param
+ * @return
+ */
+function detenerIntervalo() {
+    clearInterval(cambioturno);
+    clearInterval(reduccion);
+}
